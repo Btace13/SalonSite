8a371b16f428c48564bbc3445953ae8224df35a4
Enrique U <btace1347@yahoo.com>
2017-07-28 22:15:30 +0000

    redesign

---
+++ js/js.js
@@ -0,0 +1,24 @@
+
+(function($) {
+    "use strict";
+
+    $('body').scrollspy({
+        target: '.navbar-fixed-top',
+        offset: 60
+    });
+
+    $('#topNav').affix({
+        offset: {
+            top: 200
+        }
+    });
+    
+    new WOW().init();
+    
+    $('.navbar-collapse ul li a').click(function() {
+        /* always close responsive nav after click */
+        $('.navbar-toggle:visible').click();
+    });
+
+
+})(jQuery);
